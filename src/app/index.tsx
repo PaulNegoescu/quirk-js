9cfccd6dd3d948b5c91e4e6f7ce87969bef287c3
Paul Negoescu <paul.negoescu@ft.com>
2018-11-09 14:16:47 +0200

    Small changes and refactorings

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
 import Gana from '../views/gana';
 
-class App extends React.Component<{}, {}> {
-  public render() {
-    return (
-      <div className="gana-app container-fluid">
-        <Gana />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="gana-app container-fluid">
+    <Gana />
+  </div>
+);
 
 export default App;
